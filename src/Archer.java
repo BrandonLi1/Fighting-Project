fd2d078cb93994bc8efaf27935a55287809ff331
BT_1N3_22 <keviny119@nycstudents.net>
2025-05-30 10:34:17 -0400

    Archer class WIP

--- src/Archer.java
+++ src/Archer.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.css.Rect;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -10,6 +12,27 @@ public class Archer extends Character{
     ArrayList<BufferedImage> images = new ArrayList<>();
 
     public Archer() {
-        super("Sagittarii", 350, 1, )
+        super("Sagittarii", 350, 1, 130, 150, 4, 13, 1, 0, 300, 675, false, false, true, 6);
+        for (int i = 11; i < 22; i++) {
+            String filename = "src\\Archer\\Fire\\Archer" + i + ".png";
+            try {
+                images.add(ImageIO.read(new File(filename)));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        animation.stop();
+        attackAnimation = new Animation(images, 50, false);
+        attackAnimation.stop();
+    }
+
+    public Rectangle attack() {
+        setAnimation(attackAnimation);
+        setAttack(30, height);
+        attackAnimation.resume();
+        if (facingRight) {
+            return new Rectangle((int) (xCoord + width), (int) (yCoord), -aWidth, -aHeight);
+        }
+        return new Rectangle((int) (xCoord + width), (int) (yCoord), aWidth, aHeight);
     }
 }
