66846669618bc3627917540977b6182834d738d6
BT_1N3_22 <keviny119@nycstudents.net>
2025-06-02 11:02:01 -0400

    updated archer class and added selection pic

--- src/Archer.java
+++ src/Archer.java
@@ -3,24 +3,80 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class Archer extends Character{
     private Animation attackAnimation;
+    private Animation runAnimation;
+    private Animation deathAnimation;
+    private Animation hurtAnimation;
+    private Animation idleAnimation;
     ArrayList<BufferedImage> images = new ArrayList<>();
 
     public Archer() {
         super("Sagittarii", 350, 1, 130, 150, 4, 13, 1, 0, 300, 675, false, false, true, 6);
+
+        //Archer attack
         for (int i = 11; i < 22; i++) {
-            String filename = "src\\Archer\\Fire\\Archer" + i + ".png";
+            String filename = "src\\Archer\\Attack\\Archer0" + i + ".png";
             try {
                 images.add(ImageIO.read(new File(filename)));
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             }
         }
-        animation.stop();
         attackAnimation = new Animation(images, 50, false);
+        images = new ArrayList<>();
+
+        //Archer run
+        for (int i = 22; i < 30; i++) {
+            String filename = "src\\Archer\\Run\\Archer0" + i + ".png";
+            try {
+                images.add(ImageIO.read(new File(filename)));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        runAnimation = new Animation(images, 50, false);
+        images = new ArrayList<>();
+
+        //Archer idle
+        for (int i = 0; i < 5; i++) {
+            String filename = "src\\Archer\\Idle\\Archer00" + i + ".png";
+            try {
+                images.add(ImageIO.read(new File(filename)));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        idleAnimation = new Animation(images, 50, true);
+        images = new ArrayList<>();
+
+        //Archer death
+        for (int i = 44; i < 50; i++) {
+            String filename = "src\\Archer\\Death\\Archer0" + i + ".png";
+            try {
+                images.add(ImageIO.read(new File(filename)));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        deathAnimation = new Animation(images, 50, false);
+        images = new ArrayList<>();
+
+        //Archer hurt
+        for (int i = 33; i < 38; i++) {
+            String filename = "src\\Archer\\Hurt\\Archer0" + i + ".png";
+            try {
+                images.add(ImageIO.read(new File(filename)));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        hurtAnimation = new Animation(images, 50, false);
+
+        animation.stop();
         attackAnimation.stop();
     }
 
